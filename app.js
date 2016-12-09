0ae40c381e6088e3a038dc49a30cc49cc38ea122
amfang <JackyFang@jackydeair.cn.ibm.com>
2016-12-09 10:37:28 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,4 +1,52 @@
+/*
+ var express = require("express");
 
+ // cfenv provides access to your Cloud Foundry environment
+ // for more info, see: https://www.npmjs.com/package/cfenv
+ var cfenv = require("cfenv");
+
+ // create a new express server
+ var app = express();
+
+ //add http&socket module
+ var http = require("http").Server(app);
+ var io = require("socket.io")(http);
+
+ // serve the files out of ./public as our main files
+ app.use(express.static(__dirname + "/public"));
+
+
+ io.on("connection",function(socket){
+
+ socket.on("String",function(key){
+ if(key == "w" || key == "s" || key == "a" || key == "d"){
+ console.log("Received " + key + " from frontend");
+
+ //************************
+ //fill in a method "io.sockets.emit(param1,param2)" in line 35
+ //The first parameter is the data type "message", the second parameter is the data you want to send
+ io.sockets.emit("message", key);
+ }
+ else{
+ console.log("Not a direction order");
+ }
+ });
+
+ });
+
+ // get the app environment from Cloud Foundry
+ var appEnv = cfenv.getAppEnv();
+
+ // start server on the specified port and binding host
+
+ http.listen(appEnv.port, "0.0.0.0", function() {
+ // print a message when the server starts listening
+ console.log("server starting on " + appEnv.url + " " + appEnv.port);
+ });
+
+ */
+
+/*
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -6,6 +54,22 @@ var io = require('socket.io')(http);
 app.get('/', function(req, res){
 	res.send('<h1>Welcome Realtime Server</h1>');
 });
+*/
+var express = require("express");
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require("cfenv");
+
+// create a new express server
+var app = express();
+
+//add http&socket module
+var http = require("http").Server(app);
+var io = require("socket.io")(http);
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + "/public"));
 
 //在线用户
 var onlineUsers = {};
@@ -59,7 +123,18 @@ io.on('connection', function(socket){
 
 });
 
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+// start server on the specified port and binding host
+
+http.listen(appEnv.port, "0.0.0.0", function() {
+	// print a message when the server starts listening
+	console.log("server starting on " + appEnv.url + " " + appEnv.port);
+});
+
+/*
 http.listen(3000, function(){
 	console.log('listening on *:3000');
 });
-
+*/
